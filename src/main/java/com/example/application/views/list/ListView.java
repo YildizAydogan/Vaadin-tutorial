3dadbd880d4cbc79ce70c0077e53d3c82857aa03
Yildiz <aydogannyildiz@gmail.com>
2024-03-06 14:04:11 +0100

    06.03.24

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,33 +1,67 @@
 package com.example.application.views.list;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.example.application.data.Contact;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
-@PageTitle("list")
+import java.awt.*;
+
+@PageTitle("Contacts | Vaadin CRM")
 @Route(value = "")
 public class ListView extends VerticalLayout {
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
 
     public ListView() {
-        setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+        addClassName("list-view");
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+
+
+        configureGrid();
+
+        add(
+                getToolbar(),
+                grid
+        );
+
+    }
+
+    private Component getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY); // kullanıcı arama çubuğuna sürekli harfler girdiğinde, bu mod aktif olduğunda sistem bir süre bekler (genellikle birkaç saniye ya da kullanıcı yazmayı durdurduktan sonra) ve daha sonra olayı tetikler. Bu şekilde, kullanıcının girişi tamamlandıktan sonra bir kez sunucuya istek yapılır. Bu, gereksiz sunucu isteklerini azaltarak uygulamanın genel performansını artırır.
+
+
+        Button addContactButton = new Button("Add Contact");
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+
+        return toolbar;
+
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email");
+        grid.addColumn(contact-> contact.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact-> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(col->col.setAutoWidth(true));
+
+
     }
 
 }
